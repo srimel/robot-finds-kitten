46c8b207f299617f1c526aade0c1808fa694f822
Stuart Rimel <stuart.rimel@gmail.com>
2023-07-01 16:49:32 -0700

    Add window class to manage windows

---
+++ RobotFindsKitten/Components/Window.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Mindmagma.Curses;
+
+namespace RobotFindsKitten.Components
+{
+	public class Window
+	{
+		public IntPtr WindowPtr { get; }
+		public bool IsSubWindow { get; } = false;
+		public int xMax { get; }
+		public int yMax { get; }
+
+		public Window(IntPtr? parent, int rows, int columns, int yOrigin, int xOrigin)
+		{
+			if (parent.HasValue)
+			{ 
+				WindowPtr = NCurses.SubWindow(parent.Value, rows, columns, xOrigin, yOrigin);
+				IsSubWindow = true;
+			}
+			else 
+			{
+				WindowPtr = NCurses.NewWindow(rows, columns, yOrigin, xOrigin);
+			}
+			int _yMax, _xMax;
+			NCurses.GetMaxYX(WindowPtr, out _yMax, out _xMax);
+			yMax = _yMax;
+			xMax = _xMax;
+		}
+
+		public void Refresh()
+		{
+			NCurses.WindowRefresh(WindowPtr);
+		}
+
+		public void GetChar()
+		{
+			NCurses.WindowGetChar(WindowPtr);
+		}
+
+		public void ShowBorder(char vert, char horz)
+		{
+			NCurses.Box(WindowPtr, vert, horz);
+		}
+
+		public void MoveAddChar(int y, int x, char c)
+		{
+			NCurses.MoveWindow(WindowPtr, y, x);
+			NCurses.WindowAddChar(WindowPtr, c);
+		}
+
+		public void MoveAddStr(int y, int x, string msg)
+		{
+			NCurses.MoveWindowAddString(WindowPtr, y, x, msg);
+		}
+	}
+}
+
--- RobotFindsKitten/Program.cs
+++ RobotFindsKitten/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Mindmagma.Curses;
+using RobotFindsKitten.Components;
 
 namespace RobotFindsKitten;
 
@@ -17,18 +18,17 @@ class Program
         NCurses.AddString(screenDimens);
         NCurses.Refresh();
 
-        IntPtr window = NCurses.NewWindow(yMax/2, xMax/2, yMax/4, xMax/4);
-        NCurses.Box(window, (char) 0, (char) 0);
-        int yMaxWin, xMaxWin;
-        NCurses.GetMaxYX(window, out yMaxWin, out xMaxWin);
-        NCurses.MoveWindowAddString(window, 1, 1, $"Window Dimensions Y: {yMaxWin}, X: {xMaxWin}");
-        NCurses.WindowRefresh(window);
 
-        int finalMessageRow = yMax / 4 + yMaxWin + 3;
+        Window window = new (null, yMax / 2, xMax / 2, yMax / 4, xMax / 4);
+        window.ShowBorder((char) 0, (char) 0);
+        window.MoveAddStr(1, 1, $"Window Dimensions Y: {window.yMax}, X: {window.xMax}");
+        window.Refresh();
+
+        int finalMessageRow = yMax / 4 + window.yMax + 3;
         NCurses.MoveAddString(finalMessageRow, 0, "Press any key to end program...");
         NCurses.Refresh();
 
-        NCurses.WindowGetChar(window);
+        window.GetChar();
         NCurses.EndWin();
     }
 }
