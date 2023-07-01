6595e2af9e9cbd64f8d13d7bae4cdba44bbadb52
Stuart Rimel <stuart.rimel@gmail.com>
2023-07-01 02:37:05 -0700

    implement simple hello curses program

--- RobotFindsKitten/Program.cs
+++ RobotFindsKitten/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace RobotFindsKitten;
+﻿using Mindmagma.Curses;
+
+namespace RobotFindsKitten;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string welcome = "Hello Curses!";
+
+        var screen = NCurses.InitScreen();
+
+        // Simple animated welcome message
+        for (int i = 0; i < welcome.Length; i++)
+        {
+            NCurses.MoveAddChar(0, i, welcome[i]);
+            NCurses.Refresh();
+            NCurses.Nap(100);
+		}
+
+        // Print a box of chars
+        int ROWS = 3;
+        int COLS = 5;
+        int STARTROW = 3;
+        for (int i = STARTROW; i < ROWS + STARTROW; i++)
+        { 
+            for (int j = 0; j < COLS; j++)
+            {
+                NCurses.MoveAddChar(i, j, 'o');
+			}
+		}
+
+        int ENDROW = ROWS + STARTROW + 1;
+        NCurses.Move(ENDROW, 0);
+        NCurses.AddString("Press any key to end program. ");
+
+        NCurses.Refresh();
+        NCurses.GetChar();
+        NCurses.EndWin();
     }
 }
 
