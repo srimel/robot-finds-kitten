4bbfe5017c7f4dcbaa601d3fbed453ed21acd5f7
Stuart Rimel <stuart.rimel@gmail.com>
2023-07-03 20:51:12 -0700

    Add GameApp class

---
+++ RobotFindsKitten/GameApp.cs
@@ -0,0 +1,90 @@
+﻿using Mindmagma.Curses;
+using RobotFindsKitten.Components;
+
+namespace RobotFindsKitten
+{
+    public class GameApp
+    {
+        private readonly Screen screen;
+        private readonly Window infoWin;
+        private readonly Window mainWin;
+
+        private int finalMessageRow;
+
+        public GameApp()
+        {
+            screen = new();
+
+            infoWin = new(null,
+                screen.ROWS / 8,
+                screen.COLS / 2,
+                screen.ROWS / 8,
+                screen.COLS / 4);
+
+            mainWin = new(null,
+                screen.ROWS / 2,
+                screen.COLS / 2,
+                screen.ROWS / 4,
+                screen.COLS / 4);
+        }
+
+        public void Run()
+        { 
+			Init();
+			TestMessageEndProgram();
+			TestAutoMoveWindowCursor();
+			TestMoveWindowInspect();
+			GetCharEndProgram();
+		}
+
+        public void Init()
+        {
+            screen.ApplySettings();
+            screen.SetTitle("Robot Finds Kitten");
+            infoWin.ShowBorder((char)0, (char)0);
+            mainWin.ShowBorder((char)0, (char)0);
+            mainWin.PopulateWindow();
+        }
+
+        public void GetCharEndProgram()
+        { 
+			mainWin.GetChar();
+			NCurses.EndWin();
+		}
+
+        public void TestMessageEndProgram()
+        {
+            finalMessageRow = screen.ROWS / 4 + mainWin.ROWS + 3;
+            NCurses.AttributeOn(NCurses.ColorPair(3));
+            NCurses.MoveAddString(finalMessageRow, 0, "Press any key to end program...");
+            NCurses.AttributeOff(NCurses.ColorPair(3));
+            NCurses.Refresh();
+        }
+
+        public void TestAutoMoveWindowCursor()
+        {
+            // Test for moving cursor within in window (automated)
+            for (int i = 1; i < mainWin.ROWS - 1; i++)
+            {
+                for (int j = 1; j < mainWin.COLS - 1; j++)
+                {
+                    //NCurses.WindowMove(window.WindowPtr, i, j);
+                    mainWin.MoveCursor(i, j);
+                    mainWin.Refresh();
+                    NCurses.Nap(10);
+                }
+            }
+        }
+
+        public void TestMoveWindowInspect()
+        { 
+			// Example of inspecting char at given cursor position
+			var result = NCurses.MoveWindowInspectChar(mainWin.WindowPtr, 1, 1);
+			int resultColorPair = (int)((result & CursesAttribute.COLOR) >> 8);
+			char resultChar = (char)(result & CursesAttribute.CHARTEXT);
+			NCurses.MoveAddString(finalMessageRow + 3, 0, $"First char of window: {resultChar}\nColor Pair: {resultColorPair}");
+			NCurses.Refresh();
+		}
+    }
+}
+
--- RobotFindsKitten/Program.cs
+++ RobotFindsKitten/Program.cs
@@ -7,44 +7,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Screen screen = new();
-        screen.ApplySettings();
-        screen.SetTitle("Robot Finds Kitten");
-
-        Window info = new(null, screen.ROWS / 8, screen.COLS / 2, screen.ROWS / 8, screen.COLS / 4);
-        info.ShowBorder((char)0, (char)0);
-
-        Window window = new(null, screen.ROWS / 2, screen.COLS / 2, screen.ROWS / 4, screen.COLS / 4);
-        window.ShowBorder((char)0, (char)0);
-        window.PopulateWindow();
-
-        int finalMessageRow = screen.ROWS / 4 + window.ROWS + 3;
-        NCurses.AttributeOn(NCurses.ColorPair(3));
-        NCurses.MoveAddString(finalMessageRow, 0, "Press any key to end program...");
-        NCurses.AttributeOff(NCurses.ColorPair(3));
-        NCurses.Refresh();
-
-        // Test for moving cursor within in window (automated)
-        for (int i = 1; i < window.ROWS - 1; i++)
-        {
-            for (int j = 1; j < window.COLS - 1; j++)
-            {
-                //NCurses.WindowMove(window.WindowPtr, i, j);
-                window.MoveCursor(i, j);
-                window.Refresh();
-                NCurses.Nap(10);
-            }
-        }
-
-        // Example of inspecting char at given cursor position
-        var result = NCurses.MoveWindowInspectChar(window.WindowPtr, 1, 1);
-        int resultColorPair = (int)((result & CursesAttribute.COLOR) >> 8);
-        char resultChar = (char)(result & CursesAttribute.CHARTEXT);
-        NCurses.MoveAddString(finalMessageRow + 3, 0, $"First char of window: {resultChar}\nColor Pair: {resultColorPair}");
-        NCurses.Refresh();
-
-        window.GetChar();
-        NCurses.EndWin();
+        GameApp gameApp = new();
+        gameApp.Run();
     }
 }
 
