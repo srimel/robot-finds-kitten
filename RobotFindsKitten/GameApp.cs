ffa8dfa1a2bd2d532ac552415e265963f54e725d
Stuart Rimel <stuart.rimel@gmail.com>
2023-07-06 10:33:10 -0700

    add string library

--- RobotFindsKitten/GameApp.cs
+++ RobotFindsKitten/GameApp.cs
@@ -9,6 +9,9 @@ namespace RobotFindsKitten
         private readonly Window infoWin;
         private readonly Window mainWin;
         private List<Tuple<int, int>> objects;
+        private List<string> stringLibrary;
+        private List<int> stringMapping;
+        private int kitten;
 
         public GameApp()
         {
@@ -25,12 +28,13 @@ namespace RobotFindsKitten
                 screen.COLS / 2,
                 screen.ROWS / 4,
                 screen.COLS / 4);
+
+            Init();
         }
 
         public void Run()
         {
-            Init();
-            int key = 0;
+            int key;
             while ((key = NCurses.GetChar()) != CursesKey.ESC)
             {
                 NCurses.GetYX(mainWin.WindowPtr, out int currY, out int currX);
@@ -39,7 +43,17 @@ namespace RobotFindsKitten
                     case CursesKey.LEFT:
                         if (currX > 1)
                         {
-                            mainWin.MoveCursor(currY, currX - 1);
+                            int x;
+                            if ((x = GetObjectIndex(currY, currX - 1)) >= 0)
+                            {
+                                infoWin.Clear();
+                                AddInfoString(infoWin, stringLibrary[stringMapping[x]]);
+                                infoWin.Refresh();
+                            }
+                            else
+                            {
+                                mainWin.MoveCursor(currY, currX - 1);
+                            }
                         }
                         break;
                     case CursesKey.RIGHT:
@@ -75,7 +89,88 @@ namespace RobotFindsKitten
             mainWin.ShowBorder((char)0, (char)0);
             mainWin.PopulateWindow();
             objects = mainWin.Objects;
+            HideKitten();
+            GenerateStringLibrary();
+            MapStrings();
         }
+
+        private void HideKitten()
+        {
+            Random rng = new();
+            kitten = rng.Next(0, objects.Count);
+        }
+
+        private bool IsKitten(int y, int x)
+        {
+            return y == objects[kitten].Item1 && x == objects[kitten].Item2;
+        }
+
+        // Returns -1 if no object is at coordinates
+        private int GetObjectIndex(int y, int x)
+        {
+            for (int i = 0; i < objects.Count; i++)
+            {
+                if (y == objects[i].Item1 && x == objects[i].Item2)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        // Game idea: find cat treats which gives a bonus somehow
+        // Maybe write all of these as the player's thoughts as their walking around. (write up about this)
+        private void GenerateStringLibrary()
+        {
+            stringLibrary = new()
+            {
+                "Huh, an old floppy disc. It's soaking wet!",
+                "Here's some old fishing gear, it smells really bad.",
+                "This is someone's punch card. They better not be late for work!",
+                "I found a cathode-ray tube. Why is it glowing omniously? I should probably keep walking.",
+                "Wow, a prestine Smith Corona typewriter! I can finally write my novel.",
+                "A copy VHS copy of Raiders of the Lost Ark! I'll watch this later.",
+                "Here is an old film reel-to-reel film projector, looks out of commission.",
+                "I found a glorious abacus! I'll be a great mathematician someday!",
+                "Oh god, I stepped on a vinyl copy of Kraftwerk. I wonder if it will still play?",
+                "Ooo, I found a gold pocket watch laying a puddle of mud. Maybe I can clean it up.",
+                "Ahh, here's an open can of tuna, it has all been eaten! I must be getting close!",
+                "Is this a rotary telephone? * picks up reciever * Hello? * distant meowing is heard on the other line *",
+                "There's a dot matrix printer, it looks like it's working on rendering a mandlebrot set. Very cool."
+            };
+        }
+
+        private void MapStrings()
+        {
+
+            stringMapping = new();
+            Random rng = new();
+            while (stringMapping.Count != objects.Count)
+            {
+                var x = rng.Next(0, stringLibrary.Count);
+                if (!stringMapping.Contains(x))
+                {
+                    stringMapping.Add(x);
+                }
+            }
+
+        }
+
+        private void AddInfoString(Window win, string msg)
+        {
+            int s = 0;
+            for (int i = 1; i < win.ROWS - 1; i++)
+            { 
+                for (int j = 1; j < win.COLS - 1; j++)
+                {
+                    if (s < msg.Length)
+                    { 
+						NCurses.MoveWindowAddChar(win.WindowPtr, i, j, msg[s]);
+						s++;
+					}
+				}
+			}
+		}
 
         public void GetCharEndProgram()
         {
